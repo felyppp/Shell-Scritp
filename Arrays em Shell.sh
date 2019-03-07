837aabac0c31eeb38e932f2574f2e16647ff483d
José Henrique <josehenrique9870@hotmail.com>
2019-03-07 20:23:25 -0300

    Add files via upload

---
+++ Arrays em Shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Olá Mundo"
+
+MUNDO=("Shell Script" "Bash" "GNU" "Linux" "Debian")
+
+echo ${mundo[0]} # Ira mostrar o primeiro que é Shell Script
+echo ${mundo[1]} # Ira mostrar o Segundo que é Bash
+echo ${mundo[2]} # Ira mostrar o Terceiro que é	GNU
+echo ${mundo[3]} # Ira mostrar o Quarto que é Linux
+echo ${mundo[4]} # Ira mostrar o Quinto que é Debian
